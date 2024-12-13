5b6ca2d1d6a001746f0230ee803e5f3f15a160dc
CAEBUD\gkisida <gkisida@cae.com>
2024-12-13 17:02:40 +0100

    added bomb blast sound

--- audio.go
+++ audio.go
@@ -3,14 +3,20 @@ package main
 import (
 	"log"
 	"os"
+	"path"
+	"io"
 
 	"github.com/hajimehoshi/ebiten/v2/audio"
 	"github.com/hajimehoshi/ebiten/v2/audio/mp3"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
 )
+
+var globalAudioContext *audio.Context
 
 type Audio struct {
 	// Theme music asset file
 	themeMusicAssetFile string
+	loopedPlay bool
 	// player
 	player *audio.Player
 	// music file
@@ -22,22 +28,48 @@ type Audio struct {
 // stream, and creates a new player for the audio context. If any error occurs
 // during these steps, the function logs the error and terminates the program.
 func (a *Audio) createMusicPlayer() {
-	audioContext := audio.NewContext(44100)
+	if globalAudioContext == nil {
+		globalAudioContext = audio.NewContext(44100)
+	}
+
 	var err error
 	a.musicFile, err = os.Open(a.themeMusicAssetFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	themeMusicStream, err := mp3.DecodeWithSampleRate(audioContext.SampleRate(), a.musicFile)
+	
+	fileExt := path.Ext(a.themeMusicAssetFile)
+
+	var audioStream io.ReadSeeker
+	audioStreamLength := int64(0)
+	if fileExt == ".mp3" {
+		mp3AudioStream, err2 := mp3.DecodeWithSampleRate(globalAudioContext.SampleRate(), a.musicFile)
+		audioStreamLength = mp3AudioStream.Length()
+		audioStream = mp3AudioStream
+		err = err2
+	} else if fileExt == ".wav" {
+		wavAudioStream, err2 := wav.DecodeWithSampleRate(globalAudioContext.SampleRate(), a.musicFile)
+		audioStreamLength = wavAudioStream.Length()
+		audioStream = wavAudioStream
+		err = err2
+	} else {
+		log.Fatalf("Unknown auido file extension '%s'", a.themeMusicAssetFile)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	loopedStream := audio.NewInfiniteLoop(themeMusicStream, themeMusicStream.Length())
 
-	lengthSec := (int)(themeMusicStream.Length()) / (audioContext.SampleRate() * 2 * 2) // 16 bits stereo
-	log.Printf("Streaming '%s' (length %d s) in infinite loop", a.themeMusicAssetFile, lengthSec)
+	audioLengthSec := (int)(audioStreamLength) / (globalAudioContext.SampleRate() * 2 * 2) // 16 bits stereo
+	log.Printf("Stream '%s' (length %d s) prepared (infinite=%t)", a.themeMusicAssetFile, audioLengthSec, a.loopedPlay)
 
-	a.player, err = audioContext.NewPlayer(loopedStream)
+	if a.loopedPlay {
+		loopedStream := audio.NewInfiniteLoop(audioStream, audioStreamLength)
+		a.player, err = globalAudioContext.NewPlayer(loopedStream)
+	} else  {
+		a.player, err = globalAudioContext.NewPlayer(audioStream)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -51,9 +83,10 @@ func (a *Audio) createMusicPlayer() {
 //
 // Returns:
 //   - *Audio: A pointer to the newly created Audio instance.
-func NewAudio(themeMusicAssetFile string) *Audio {
+func NewAudio(themeMusicAssetFile string, loopedPlay bool) *Audio {
 	a := &Audio{
 		themeMusicAssetFile: themeMusicAssetFile,
+		loopedPlay: loopedPlay,
 	}
 	a.createMusicPlayer()
 	return a
